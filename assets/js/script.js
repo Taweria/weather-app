4924c87d4302063ec289ab06e44585221eb8e70e
Taweria <elodieali.pro@gmail.com>
2023-10-11 08:49:38 +0200

    Fix error 401 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ loader.style.display = "none";
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const city = input.value;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${VITE_APIKEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`;
   
 
     loader.style.display = "block";
